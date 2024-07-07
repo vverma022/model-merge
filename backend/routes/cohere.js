4b51ae7b2e99bad98992884a8c0c544526618ec3
Vasu Verma <vermavasu069@gmail.com>
2024-07-07 15:29:05 +0530

    Added Models

---
+++ backend/routes/cohere.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import { configDotenv } from 'dotenv';
+import { CohereClient } from 'cohere-ai';
+configDotenv();
+
+const router = express.Router();
+
+const cohere = new CohereClient({
+  token: process.env.COHERE_API_KEY,
+});
+
+router.post('/chat', async (req,res) => {
+    const userMessage = req.body.message;
+
+    if(!userMessage){
+        return res.status(400).json({error: 'Message is required'});
+    }
+    try{
+        const response = await cohere.chat({
+            message: userMessage,
+        });
+
+        const aiResponse =  response.text || "";
+
+        res.json({response: aiResponse});
+    } catch(error){
+        console.error("Error fetching completion:", error);
+        res.status(500).json({error: 'An error occurred while processing the request'});
+    }
+});
+
+export default router;
+
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import cors from 'cors';
 import { configDotenv } from 'dotenv';
 import groq from './routes/groq.js';
 import gemini from './routes/gemini.js';
+import aimlapi from './routes/aimlapi.js';
+import cohere from './routes/cohere.js';
 
 configDotenv();
 
@@ -16,7 +18,8 @@ app.use(cors());
 // Routes
  app.use('/api', groq);
  app.use('/api2', gemini);
-// app.use('/api3', api3);
+ app.use('/api3', aimlapi);
+ app.use('/api4', cohere);
 
 
 app.listen(port, () => {

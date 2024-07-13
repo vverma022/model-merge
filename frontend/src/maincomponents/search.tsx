144fdaa882aa1ef4576e5eea98af32ed9092e910
Vasu Verma <vermavasu069@gmail.com>
2024-07-13 22:06:33 +0530

    Setup search component

--- frontend/src/maincomponents/search.tsx
+++ frontend/src/maincomponents/search.tsx
@@ -2,18 +2,38 @@
 import { Input } from '../components/ui/input'
 import { Button } from '../components/ui/button'
 import { SelectModel } from './selectmodel'
-import { useState } from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
+import { stat } from 'fs'
 
 
 
 const Search = () => {
   const [Prompt,SetPrompt] = useState("");
-  const [Model,SetModel] = useState("");
+  const [SelectedModel,SetSelectedModel] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    if(!SelectedModel || !Prompt) {
+      alert("Please select a model and enter a prompt");
+      return;
+    }
+    try{
+      const response = await axios.post("http://localhost:6000/api/chat",{
+        model: SelectedModel,
+        prompt: Prompt
+      });
+      console.log(response.data);
+    }catch(error){
+        console.error("Error sneding request to the server",(error as any).response.status);
+    }
+  };
 
   return (
     <div className="flex w-full flex-row p-2">
+      <form onSubmit={handleSubmit} className='flex'>
       <div>
-      <SelectModel onChange={SetModel}  />
+      <SelectModel onChange={SetSelectedModel}  />
       </div>
       <div className='w-full px-1'>
       <Input type="text" placeholder="Prompt" value={Prompt} onChange={(e) => SetPrompt(e.target.value)}/>
@@ -23,6 +43,7 @@ const Search = () => {
         Send
       </Button>
       </div>
+      </form>
     </div>
     )
   }
--- frontend/src/pages/main.tsx
+++ frontend/src/pages/main.tsx
@@ -2,6 +2,7 @@ import Sidebar2 from "../components/ui/sidebar2"
 import { Button } from "../components/ui/button"
 import { Input } from "../components/ui/input"
 import { SelectModel } from "../maincomponents/selectmodel"
+import Search from "../maincomponents/search"
 
 
 
@@ -15,19 +16,7 @@ const Main = () => {
       {/* Main Content */}
       <div className="flex-grow p-4 relative">
         <div className="absolute inset-x-0 bottom-0 w-full p-2">
-        <div className="flex w-full flex-row p-2">
-      <div>
-      <SelectModel/>
-      </div>
-      <div className='w-full px-1'>
-      <Input type="text" placeholder="Prompt"/>
-      </div>
-      <div>
-      <Button type="submit"className='w-28 bg-red-600 hover:bg-red-400 text-white'>
-        Send
-      </Button>
-      </div>
-    </div>
+        <Search />
         </div>
       </div>
     </div>

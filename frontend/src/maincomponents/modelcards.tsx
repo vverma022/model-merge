7460f47df0bfd951488f4658e771cd1097d8f2da
Vasu Verma <vermavasu069@gmail.com>
2024-07-14 16:23:27 +0530

    Fixes #4 Add Icon's to the Model Info Page

--- frontend/src/maincomponents/modelcards.tsx
+++ frontend/src/maincomponents/modelcards.tsx
@@ -1,29 +1,35 @@
 import { Card, CardHeader, CardTitle, CardContent} from '../components/ui/card'
-import { Button } from '../components/ui/button'
+import { Avatar, AvatarImage, AvatarFallback } from '../components/ui/avatar'
+import { Url } from 'url'
 
 interface ModelCardsProps {
   CardT: string,
   CardD: string,
   Company: string
+  Icon: string
 }
 
-export const ModelCards = ({CardT, CardD,Company}: ModelCardsProps) => {
+export const ModelCards = ({CardT, CardD,Company,Icon}: ModelCardsProps) => {
 
   return (
     <div className="p-4">
-    <div className="flex gap-4 mt-4 overflow-x-scroll">
-      <Card>
-        <CardHeader>
-          <CardTitle className="text-center pb-2">{CardT}</CardTitle>
-        </CardHeader>
-        <CardContent className="flex gap-4 justify-center flex-col">
-          <ul className="font-extralight text-sm text-red-600">Developed By {Company}</ul>
-          <p className="overflow-hidden text-pretty overflow-y-auto max-h-48">
-            {CardD}
-          </p>
-        </CardContent>
-      </Card>
+      <div className="flex gap-4 mt-4 overflow-x-scroll">
+        <Card>
+          <CardHeader>
+              <Avatar className='flex justify-end'>
+                  <AvatarImage src={Icon} />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+                <CardTitle className="text-center pb-2">{CardT}</CardTitle>
+          </CardHeader>
+          <CardContent className="flex gap-4 justify-center flex-col">
+            <ul className="font-extralight text-sm text-red-600">Developed By {Company}</ul>
+            <p className="overflow-hidden text-pretty overflow-y-auto max-h-48">
+              {CardD}
+            </p>
+          </CardContent>
+        </Card>
+      </div>
     </div>
-  </div>
-  )
+  );
 }

6d370cce271ca21292e7f8beb28a2bd268401c85
Vasu Verma <vermavasu069@gmail.com>
2024-07-24 22:46:43 +0530

    Add Sigin and Signup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,8 @@ import { ThemeProvider } from "./components/ui/theme-provider";
 import Main from "./pages/main";
 import Models from "./pages/models";
 import AboutUs from "./pages/about-us";
+import Signin from "./pages/signin";
+import Signup from "./pages/signup";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Welcome />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
           <Route path="/main" element={<Main typewriterDelay={50} />} />
           <Route path="/info/model" element={<Models />} />
           <Route path="/about-us" element={<AboutUs />} />
--- frontend/src/pages/signin.tsx
+++ frontend/src/pages/signin.tsx
@@ -3,20 +3,16 @@ import { Label } from "../components/ui/label"
 import { Input } from "../components/ui/input"
 import { Button } from "../components/ui/button"
 
-export default function Signup() {
+export default function Signin() {
 return (
     <div className="flex justify-center items-center h-screen">
         <Card className="mx-auto max-w-sm">
             <CardHeader className="space-y-1">
-                <CardTitle className="text-2xl font-bold">Signup</CardTitle>
-                <CardDescription>Enter your details to create your account with ModelMerge</CardDescription>
+                <CardTitle className="text-2xl font-bold">Login</CardTitle>
+                <CardDescription>Enter your email and password to login to your account</CardDescription>
             </CardHeader>
             <CardContent>
                 <div className="space-y-4">
-                <div className="space-y-2">
-                        <Label htmlFor="username">Username</Label>
-                        <Input id="username" type="text" placeholder="username" required />
-                    </div>
                     <div className="space-y-2">
                         <Label htmlFor="email">Email</Label>
                         <Input id="email" type="email" placeholder="m@example.com" required />
